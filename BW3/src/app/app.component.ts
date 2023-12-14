f7b963fe3d04b750ce0ae386e5298e4514159ba0
PaoloPascucci <pascuccip9@gmail.com>
2023-12-14 17:43:22 +0100

    Update spacing

--- BW3/src/app/app.component.ts
+++ BW3/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth/auth.service';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +9,18 @@ import { AuthService } from './auth/auth.service';
 })
 export class AppComponent implements OnInit {
   title = 'BW3';
-  constructor(private authSrv: AuthService) {}
+  isHomePage: boolean = false;
+  isProfile: boolean = false;
+  constructor(private authSrv: AuthService, private router: Router) {}
   ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        // Controlla se sei sulla homepage (percorso vuoto '/')
+        this.isHomePage = this.router.url === '/';
+        this.isProfile = this.router.url === '/posts';
+      }
+    });
+
     this.authSrv.restore();
   }
 }
