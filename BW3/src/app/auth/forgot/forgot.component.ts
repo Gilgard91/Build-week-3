9aa27c73cff669aaaee3e2cafb2b010c532f712f
Asura0107 <mircoorosio@gmail.com>
2023-12-14 11:41:41 +0100

    forgot
    forgot

--- BW3/src/app/auth/forgot/forgot.component.ts
+++ BW3/src/app/auth/forgot/forgot.component.ts
@@ -2,27 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 @Component({
   selector: 'app-forgot',
   templateUrl: './forgot.component.html',
   styleUrls: ['./forgot.component.scss'],
 })
 export class ForgotComponent implements OnInit {
+  modifyProfile: boolean = false;
+  password!: string;
+  consfirmPassword!: string;
+
   constructor(private authSrv: AuthService, private router: Router) {}
 
   ngOnInit(): void {}
-
-  accedi(form: NgForm) {
-    console.log(form.value);
-    try {
-      this.authSrv.login(form.value).subscribe();
-    } catch (error) {
-      alert('Login errato!');
-      console.log(error);
-      this.router.navigate(['/login']);
-    }
-  }
-  creaaccount() {
-    this.router.navigate(['/register']);
-  }
 }
